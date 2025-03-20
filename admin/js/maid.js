ad6d19f409dadc820f1f5677402dfa073605895a
SAIFU("RK") <150003102+saifullah2032@users.noreply.github.com>
2025-03-20 23:27:27 +0530

    Update maid.js

--- admin/js/maid.js
+++ admin/js/maid.js
@@ -2,7 +2,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
-
+// Firebase Configuration (Updated)
+const firebaseConfig = {
+    apiKey: "AIzaSyDKHHmFhtcMsMj365jMcGGSU5UfoVKT3SY",
+    authDomain: "maid-booking-4ec9e.firebaseapp.com",
+    databaseURL: "https://maid-booking-4ec9e-default-rtdb.firebaseio.com",
+    projectId: "maid-booking-4ec9e",
+    storageBucket: "maid-booking-4ec9e.appspot.com", // Fixed storage bucket
+    messagingSenderId: "59794486383",
+    appId: "1:59794486383:web:a1de68abd7a9da4945099a",
+    measurementId: "G-48143C3T7C"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
